ecee7f073e3ad62741ff27f15cd071ea89e9e63a
Theo Guillaime <guillaumetheo92@gmail.com>
2022-07-01 12:18:13 +0300

    intégration détail stock par type

---
+++ components/materiels/stock/details/table/constante.ts
@@ -0,0 +1,14 @@
+import { createData } from "./function";
+import { HeadCell } from "./type-variable";
+
+export const rows = [
+    createData('XX 00 005', 'designation_1', '12/02/2022'),
+    createData('XX 00 006', 'designation_2', '12/02/2022'),
+    createData('XX 00 007', 'designation_3', '12/02/2022'),
+    createData('XX 00 008', 'designation_4', '12/02/2022'),
+    createData('XX 00 009', 'designation_5', '12/02/2022'),
+    createData('XX 00 0010', 'designation_6', '12/02/2022'),
+    createData('XX 00 0011', 'designation_7', '12/02/2022'),
+    createData('XX 00 0012', 'designation_8', '12/02/2022'),
+    createData('XX 00 0013', 'designation_9', '12/02/2022'),
+  ];
