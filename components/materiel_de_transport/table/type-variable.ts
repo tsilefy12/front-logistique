fe5f82f6be7df8f1e20bac72e7b0e70e2d6c1e5d
CharlyEric985 <80571914+CharlyEric985@users.noreply.github.com>
2022-07-01 13:28:31 +0300

    liste de materiel de transport

---
+++ components/materiel_de_transport/table/type-variable.ts
@@ -0,0 +1,34 @@
+
+export default interface Data {
+  immatriculation: string,
+  type: string,
+  marque: string,
+  autre_information: string,
+  }
+  
+
+export type Order = 'asc' | 'desc';
+
+
+export interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+export interface EnhancedTableToolbarProps {
+  numSelected: number;
+}
+
+export interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
