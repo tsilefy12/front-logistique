5fb65b8fc4f40d64045484d953850d0144afe440
Paul Odon <46904458+PaulOdon@users.noreply.github.com>
2022-07-10 00:30:17 +0300

    fix materiel de transport back button route

--- components/Commande/eventSelect.tsx
+++
@@ -1,8 +0,0 @@
-import React,{useState} from 'react';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-const [offre, setAge] = React.useState('');
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
-  };
-  export default handleChange;
