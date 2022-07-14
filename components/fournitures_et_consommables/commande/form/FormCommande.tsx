2e027127b164c38b2ae0503fe97113e5c3b1666f
Paul Odon <46904458+PaulOdon@users.noreply.github.com>
2022-07-14 12:43:39 +0300

    fix layout and route

--- components/fournitures_et_consommables/commande/form/FormCommande.tsx
+++ components/fournitures_et_consommables/commande/form/FormCommande.tsx
@@ -16,26 +16,26 @@ import React from "react";
 import ArrowBack from "@mui/icons-material/ArrowBack";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import { Check, Close, Save } from "@mui/icons-material";
-  import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
-  const [article, setArticle] = React.useState('');
+const [article, setArticle] = React.useState("");
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setArticle(event.target.value as string);
-  };
-  const [demandeur, setDemandeur] = React.useState('');
+const handleChange = (event: SelectChangeEvent) => {
+  setArticle(event.target.value as string);
+};
+const [demandeur, setDemandeur] = React.useState("");
 
-  const handleChange_1 = (event: SelectChangeEvent) => {
-    setDemandeur(event.target.value as string);
-  };
-  
-  const FormCommande = () => {
-    return (
-      <Container maxWidth="xl" sx={{ pb: 5 }}>
-        <NavigationContainer>
+const handleChange_1 = (event: SelectChangeEvent) => {
+  setDemandeur(event.target.value as string);
+};
+
+const FormCommande = () => {
+  return (
+    <Container maxWidth="xl" sx={{ pb: 5 }}>
+      <NavigationContainer>
         <SectionNavigation>
           <Stack flexDirection={"row"}>
-            <Link href="/fournitures_et_consommables/commande/ajouter">
+            <Link href="/fournitures_et_consommables/commande">
               <Button color="info" variant="text" startIcon={<ArrowBack />}>
                 Retour
               </Button>
@@ -59,46 +59,47 @@ import { Check, Close, Save } from "@mui/icons-material";
               Annuler
             </Button>
           </Stack>
-          <Typography variant="h5">Commande des fourniture et consommable</Typography>
+          <Typography variant="h5">
+            Commande des fourniture et consommable
+          </Typography>
         </SectionNavigation>
         <Divider />
       </NavigationContainer>
 
-        
-        <FormContainer spacing={2}>
+      <FormContainer spacing={2}>
         <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Article</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={article}
-          label="Article"
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
+          <InputLabel id="demo-simple-select-label">Article</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={article}
+            label="Article"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
         <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Demandeur</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={demandeur}
-          label="Demandeur"
-          onChange={handleChange_1}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
+          <InputLabel id="demo-simple-select-label">Demandeur</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={demandeur}
+            label="Demandeur"
+            onChange={handleChange_1}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
         <Stack
-            direction="row"
-            justifyContent="flex-start"
-            alignItems="flex-start"
-            spacing={2}
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          spacing={2}
         >
           <TextField
             fullWidth
@@ -107,33 +108,40 @@ import { Check, Close, Save } from "@mui/icons-material";
             variant="outlined"
           />
           <FormControl fullWidth>
-            <TextField id="outlined-basic" label="Quantité livrée" variant="outlined" />
+            <TextField
+              id="outlined-basic"
+              label="Quantité livrée"
+              variant="outlined"
+            />
           </FormControl>
-          </Stack>
-          <FormControl fullWidth>
-            <TextField id="outlined-basic" label="Date de livraison" variant="outlined" />
-          </FormControl>
-          
-        </FormContainer>
-      </Container>
-    );
-  };
-  
-  export default FormCommande;
-  
-  export const CustomStack = styled(Stack)(({ theme }) => ({
-    [theme.breakpoints.down("sm")]: {
-      flexWrap: "wrap",
-    },
-  }));
-  
-  const FormContainer = styled(Stack)(({ theme }) => ({
-    padding: 30,
-    // border: "1px solid #E0E0E0",
-    borderRadius: 20,
-    background: "#fff",
-  }));
-  const NavigationContainer = styled(Stack)(({ theme }) => ({
+        </Stack>
+        <FormControl fullWidth>
+          <TextField
+            id="outlined-basic"
+            label="Date de livraison"
+            variant="outlined"
+          />
+        </FormControl>
+      </FormContainer>
+    </Container>
+  );
+};
+
+export default FormCommande;
+
+export const CustomStack = styled(Stack)(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    flexWrap: "wrap",
+  },
+}));
+
+const FormContainer = styled(Stack)(({ theme }) => ({
+  padding: 30,
+  // border: "1px solid #E0E0E0",
+  borderRadius: 20,
+  background: "#fff",
+}));
+const NavigationContainer = styled(Stack)(({ theme }) => ({
   flexDirection: "column",
   marginBottom: theme.spacing(2),
   flex: 1,
@@ -145,4 +153,3 @@ const SectionNavigation = styled(Stack)(({ theme }) => ({
   justifyContent: "space-between",
   paddingBottom: "5px",
 }));
-  
