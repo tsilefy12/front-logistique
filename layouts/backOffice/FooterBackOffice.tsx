876e45eb3d8980724f4422d4d539560a01ac8cda
San randry <sinrandry@gmail.com>
2022-07-15 20:52:34 +0300

    deploy on 15 july 2022

--- layouts/backOffice/FooterBackOffice.tsx
+++ layouts/backOffice/FooterBackOffice.tsx
@@ -1,29 +1,51 @@
-import { Stack, useTheme } from '@mui/material';
-import { Paper, styled, Container, Typography, Box } from '@mui/material';
-import React from 'react'
+import { Stack, useTheme } from "@mui/material";
+import {
+  Paper,
+  styled,
+  Container,
+  Typography,
+  Box,
+} from "@mui/material";
+import React from "react";
 
 const FooterBackOffice = () => {
-  const theme = useTheme()
+  const theme = useTheme();
   return (
-    <ContainerFooter sx={{ mt : 2 }}>
+    <ContainerFooter sx={{ mt: 2 }}>
       <Container maxWidth="xl">
-        <Stack spacing={0} direction="column" alignItems="center" py={1}>
-          <Typography  variant="body2" sx={{color: theme.palette.grey[500]}}>Copyright © 2022 | <Lien>HAISOA</Lien> </Typography>
-          <Typography variant="body2" sx={{color: theme.palette.grey[500]}}>Version ¤ <Lien>1.0.0</Lien>, dernière mise à jour : <Lien>07/07/2022</Lien> </Typography>
+        <Stack
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          py={1}
+        >
+          <Typography
+            variant="body2"
+            sx={{ color: theme.palette.grey[500] }}
+          >
+            Copyright © 2022 | <Lien>HAISOA</Lien>{" "}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{ color: theme.palette.grey[500] }}
+          >
+            Version ¤ <Lien>1.0.0</Lien>, dernière mise à jour :{" "}
+            <Lien>15/07/2022</Lien>{" "}
+          </Typography>
         </Stack>
       </Container>
     </ContainerFooter>
-  )
-}
+  );
+};
 
-export default FooterBackOffice
+export default FooterBackOffice;
 
 const Lien = styled("span")(({ theme }) => ({
   color: theme.palette.secondary.main,
   cursor: "pointer",
-  "&:hover":{
-    color: theme.palette.info.main
-  }
+  "&:hover": {
+    color: theme.palette.info.main,
+  },
 }));
 
 const ContainerFooter = styled(Paper)(({ theme }) => ({
@@ -31,5 +53,5 @@ const ContainerFooter = styled(Paper)(({ theme }) => ({
   // position: "absolute",
   bottom: 0,
   width: "100%",
-  borderRadius: 0
+  borderRadius: 0,
 }));
