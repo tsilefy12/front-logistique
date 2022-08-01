16e528b74034791c6101071e82ee7d18420e8bc4
Paul Odon <46904458+PaulOdon@users.noreply.github.com>
2022-08-01 14:15:38 +0300

    delete head page in materiels

--- pages/materiels/detenteur/ajouter/index.tsx
+++ pages/materiels/detenteur/ajouter/index.tsx
@@ -1,11 +1,9 @@
 import { Container } from "@mui/material";
 import type { NextPage } from "next";
 import BackOfficeLayout from "../../../../layouts/backOffice";
-import useBasePath from "../../../../hooks/useBasePath";
 import ListFormDetationMateriel from "../../../../components/materiels/detenteur/ajouter/ListFormDetationMateriel";
 
 const Detenteur: NextPage = () => {
-	const basePath = useBasePath();
 	return (
 		<BackOfficeLayout>
 			<Container maxWidth="xl">
