c74f0f7220b2f03527d8962d99898b657642b433
San randry <sinrandry@gmail.com>
2022-08-22 08:16:19 +0300

    review code

--- redux/store.ts
+++ redux/store.ts
@@ -13,11 +13,7 @@ import { notificationSlice } from "./features/notification/notificationSlice";
 import { transportationEquipmentSlice } from "./features/transportation_equipment/transportationEquipmentSlice";
 import { fournisseurSlice } from "./features/fournisseur/fournisseurSlice";
 import { orderEquipmentSlice } from "./features/orderEquipment/orderEquipmentSlice";
-<<<<<<< Updated upstream
-=======
-import { carVoucherSlice } from "./features/car-voucher/carVoucherSlice";
 import { consumptionInvoiceSlice } from "./features/consumption_invoice/consumptionInvoiceSlice";
->>>>>>> Stashed changes
 
 export const store = configureStore({
   reducer: {
@@ -31,11 +27,7 @@ export const store = configureStore({
     orderEquipment: orderEquipmentSlice.reducer,
     fournisseur: fournisseurSlice.reducer,
     transportationEquipment: transportationEquipmentSlice.reducer,
-<<<<<<< Updated upstream
-=======
-    carVoucher: carVoucherSlice.reducer,
     consumptionInvoice: consumptionInvoiceSlice.reducer,
->>>>>>> Stashed changes
   },
 });
 
