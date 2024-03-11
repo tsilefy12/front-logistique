ac905410831ea3fdb26e6ba7015c07ad6b8e7b51
Santatra <santatra00@gmail.com>
2024-03-11 15:42:32 +0300

    fetch:new_inventaire

---
+++ components/materiels/inventaire/creer/index.tsx
@@ -0,0 +1,109 @@
+import {
+    Button,
+    Container,
+    styled,
+    Typography,
+    TextField,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Stack,
+    Divider,
+  } from "@mui/material";
+  import Link from "next/link";
+  import React from "react";
+  import ArrowBack from "@mui/icons-material/ArrowBack";
+  import Close from "@mui/icons-material/Close";
+  import Check from "@mui/icons-material/Check";
+  
+  const FormInventaireMateriel = () => {
+    return (
+      <Container maxWidth="xl" sx={{ pb: 5 }}>
+        <NavigationContainer>
+          <SectionNavigation>
+            <Stack flexDirection={"row"}>
+              <Link href="/materiels/mes_commandes">
+                <Button color="info" variant="text" startIcon={<ArrowBack />}>
+                  Retour
+                </Button>
+              </Link>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                startIcon={<Check />}
+                sx={{ marginInline: 3 }}
+              >
+                Enregistrer
+              </Button>
+              <Button
+                variant="text"
+                color="warning"
+                size="small"
+                startIcon={<Close />}
+                sx={{ marginInline: 3 }}
+              >
+                Annuler
+              </Button>
+            </Stack>
+            <Typography variant="h4">Créer Inventaire</Typography>
+          </SectionNavigation>
+          <Divider />
+        </NavigationContainer>
+        <FormContainer spacing={2}>
+          <TextField
+            id="outlined-basic"
+            type="date"
+            variant="outlined"
+          />
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Demandeur</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Demandeur"
+            >
+              <MenuItem value={10}>Demandeur_1</MenuItem>
+              <MenuItem value={20}>Demandeur_2</MenuItem>
+              <MenuItem value={30}>Demandeur_3</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            id="outlined-basic"
+            type="text"
+            label="Raison de la commande"
+            variant="outlined"
+          />
+        </FormContainer>
+      </Container>
+    );
+  };
+  
+  export default FormInventaireMateriel;
+  
+  export const CustomStack = styled(Stack)(({ theme }) => ({
+    [theme.breakpoints.down("sm")]: {
+      flexWrap: "wrap",
+    },
+  }));
+  
+  const FormContainer = styled(Stack)(({ theme }) => ({
+    padding: 30,
+    borderRadius: 20,
+    background: "#fff",
+  }));
+  
+  const NavigationContainer = styled(Stack)(({ theme }) => ({
+    flexDirection: "column",
+    marginBottom: theme.spacing(2),
+    flex: 1,
+    width: "100%",
+  }));
+  
+  const SectionNavigation = styled(Stack)(({ theme }) => ({
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: "5px",
+  }));
+  
