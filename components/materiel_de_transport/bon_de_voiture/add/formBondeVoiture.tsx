ed6cf75bbe478a56a3c6e07b531f7b908807e8ee
BoutaSafidinirina <safidinirinaraz@gmail.com>
2024-04-05 13:54:11 +0300

    feat:value activite

--- components/materiel_de_transport/bon_de_voiture/add/formBondeVoiture.tsx
+++ components/materiel_de_transport/bon_de_voiture/add/formBondeVoiture.tsx
@@ -26,11 +26,10 @@ const FormBonDeVoiture = () => {
     const [ idDelete,setIdDelete] = React.useState < any[]> ([])
 
     const { isEditing, carVoucher } = useAppSelector((state) => state.carVoucher);
-    const { isEdit, activity } = useAppSelector((state) =>state.activity);
     React.useEffect(() => {
         if (id) {
-        dispatch(editCarVoucher({ id }));
-        dispatch(editActivity({ id }));
+            dispatch(editCarVoucher({ id }));
+            dispatch(editActivity({ id }));
         }
     }, [id]);
   
@@ -75,14 +74,13 @@ const FormBonDeVoiture = () => {
             enableReinitialize = {isEditing ? true : false}
             initialValues={{
                 materiel: isEditing ? carVoucher?.materiel : "",
-                date: isEditing ? carVoucher?.date : new Date(),
+                date: isEditing && carVoucher?.date ? new Date(carVoucher?.date) : new Date(),
                 reference: isEditing ? carVoucher?.reference: "",
-                activite:isEdit ? activity?.activite: "",
-                nombre:isEdit ? activity?.nombre :"",
-                pu:isEdit ? activity?.pu:"",
-                montants:isEdit ? activity?.montants: "",
+                activite: "",
+                nombre:"",
+                pu:"",
+                montants:"",
                 quantite:""
-
             }}
             validationSchema={Yup.object({
                 materiel: Yup.string().required("Veuillez remplir le champ matériel"),
