42c1c78a97cc9f54dd661048e95d1057ca1cc7e4
tsilefy12 <tsilefyandriamalaza@gmail.com>
2024-09-06 08:32:24 +0500

    fix: axios

--- redux/features/file/transportation_equipment/useCases/updateTransportEquipment.ts
+++ redux/features/file/transportation_equipment/useCases/updateTransportEquipment.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { axios } from "../../../../lib/axios";
-import { enqueueSnackbar } from "../../notification/notificationSlice";
 import { TransportationEquipmentItem } from "../transportationEquipment.interface";
+import { axios } from "../../../../../lib/axios";
+import { enqueueSnackbar } from "../../../notification/notificationSlice";
 
 /**
  * update a transportationEquipment
@@ -12,26 +12,29 @@ import { TransportationEquipmentItem } from "../transportationEquipment.interfac
  * @description : This function is used to update a transportationEquipment
  */
 
- export const updateTransportationEquipment = createAsyncThunk(
-	"transportation-equipment/updateTransportationEquipment",
-	async (data: { id: string; transportationEquipment: TransportationEquipmentItem }, thunkAPI) => {
-		try {
-			const response = await axios.patch(
-				`/logistique/transportation-equipment/${data.id}`,
-				data.transportationEquipment
-			);
-			thunkAPI.dispatch(
-				enqueueSnackbar({
-					message: "Transportation Equipment updated successfully",
-					options: { variant: "success" },
-				})
-			);
-			return response.data;
-		} catch (error: any) {
-			if (error.response) {
-				return thunkAPI.rejectWithValue(error);
-			}
-			return error;
-		}
-	}
+export const updateTransportationEquipment = createAsyncThunk(
+  "transportation-equipment/updateTransportationEquipment",
+  async (
+    data: { id: string; transportationEquipment: TransportationEquipmentItem },
+    thunkAPI
+  ) => {
+    try {
+      const response = await axios.patch(
+        `/logistique/transportation-equipment/${data.id}`,
+        data.transportationEquipment
+      );
+      thunkAPI.dispatch(
+        enqueueSnackbar({
+          message: "Transportation Equipment updated successfully",
+          options: { variant: "success" },
+        })
+      );
+      return response.data;
+    } catch (error: any) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error);
+      }
+      return error;
+    }
+  }
 );
