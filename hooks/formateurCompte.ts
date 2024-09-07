ce0d49dcbbef71ed0c2ee991b820189c652fc9b6
tsilefy12 <tsilefyandriamalaza@gmail.com>
2024-09-07 10:24:06 +0500

    fix: vendor

--- hooks/formateurCompte.ts
+++ hooks/formateurCompte.ts
@@ -5,10 +5,6 @@ export default function insertSeparatorAtMiddle(
   if (!rib) return "";
   const ribStr = rib.toString();
 
-  if (ribStr.length !== 16) {
-    throw new Error("Le numéro RIB doit contenir exactement 16 caractères.");
-  }
-
   const ribWithSeparator = ribStr.replace(/(.{4})/g, `$1${separator}`).trim();
 
   return ribWithSeparator;
